a5336703d9312b1c7fca441a0075acb31982ca96
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-05 16:21:27 -0500

    add download()

--- download.js
+++ download.js
@@ -1,7 +1,20 @@
+var download = function(p1,p2) {
+  var el = document.createElement("A");
+  el.setAttribute("href","data:application,"+p1);
+  el.setAttribute("download",p2);    
+  if (document.createEvent) {
+      var event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      el.dispatchEvent(event);
+    }
+  else {
+      el.click();
+    }
+};
 if (config.type === "web") {
-// web.html
+  // web.html
 
 } else if (config.type === "spimusic") {
-// music.js
+  // music.js
 
 }
